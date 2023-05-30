32f6e9a164c1ca3ac3ebe91d683ec403a6231443
Muhammet95 <go.suits.3@gmail.com>
2023-05-30 22:07:04 +0300

    modified player

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
 let playlist;
 let currentPlay = 0, playerStatus = 'pause';
+let audio;
 
 document.addEventListener("DOMContentLoaded", () => {
     getPlayList();
 
-    let audio = new Audio();
+    audio = new Audio();
     audio.addEventListener('ended', nextMusic);
 
 
@@ -13,47 +14,54 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('img#prev').addEventListener('click', prevMusic);
 
     document.querySelector('img#next').addEventListener('click', nextMusic);
-
-    function playPause() {
-        if (!audio.src) {
-            if (playlist) {
-                audio.src = playlist[currentPlay].path;
-            }
-            setTimeout(playPause, 100);
-        } else {
-            if (playerStatus === 'pause')
-                playMusic();
-            else
-                pauseMusic();
-        }
-    }
-
-    function nextMusic() {
-        pauseMusic();
-        currentPlay = (currentPlay + 1 > 4) ? 0 : currentPlay + 1;
-        audio.src = playlist[currentPlay].path;
-        playMusic();
-    }
-
-    function prevMusic() {
-        pauseMusic();
-        currentPlay = (currentPlay - 1 < 0) ? 4 : currentPlay - 1;
-        audio.src = playlist[currentPlay].path;
-        playMusic();
-    }
-
-    function playMusic() {
-        audio.play();
-        playerStatus = 'play';
-        document.querySelector("#player .artist").innerHTML = playlist[currentPlay].artist;
-        document.querySelector("#player .track").innerHTML = playlist[currentPlay].track;
-    }
-
-    function pauseMusic() {
-        audio.pause();
-        playerStatus = 'pause';
-    }
 });
+
+function playPause() {
+    if (!audio.src) {
+        if (playlist) {
+            audio.src = playlist[currentPlay].path;
+        }
+        setTimeout(playPause, 100);
+    } else {
+        if (playerStatus === 'pause')
+            playMusic();
+        else
+            pauseMusic();
+    }
+}
+
+function nextMusic() {
+    pauseMusic();
+    currentPlay = (currentPlay + 1 > 4) ? 0 : currentPlay + 1;
+    audio.src = playlist[currentPlay].path;
+    playMusic();
+}
+
+function prevMusic() {
+    pauseMusic();
+    currentPlay = (currentPlay - 1 < 0) ? 4 : currentPlay - 1;
+    audio.src = playlist[currentPlay].path;
+    playMusic();
+}
+
+function changeMusic(index) {
+    pauseMusic();
+    currentPlay = index;
+    audio.src = playlist[currentPlay].path;
+    playMusic();
+}
+
+function playMusic() {
+    audio.play();
+    playerStatus = 'play';
+    document.querySelector("#player .artist").innerHTML = playlist[currentPlay].artist;
+    document.querySelector("#player .track").innerHTML = playlist[currentPlay].track;
+}
+
+function pauseMusic() {
+    audio.pause();
+    playerStatus = 'pause';
+}
 
 function getPlayList() {
     fetch(window.origin + '/playlist', {
@@ -72,6 +80,7 @@ function appendPlaylist() {
         const item = document.createElement('div');
         item.style = 'width: calc(100% - 20px); display: flex; gap: 20px; align-items: center; margin: 10px; border: 1px solid lightgrey; cursor: pointer; border-radius: 3px; overflow: hidden;';
         item.dataset.trackid = index;
+        item.setAttribute('onclick', `changeMusic(${index})`);
         item.innerHTML = `<img src="${playlist[index]['image']['small']}" alt="" style="height: 50px;width: 50px;">
                         <div style="display: flex; flex-direction: column;align-items: center; width: calc(100% - 80px);">
                             <span style="font-size: 20px;">${playlist[index]['artist']}</span>
